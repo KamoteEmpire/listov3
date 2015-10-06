c17f70e0dc66a4a32ba83e5a1d8a1b3a72b396dd
sharmaine manuel <201010305@gordoncollegeccs.edu.ph>
2015-10-06 20:14:49 +0800

    Update add_affectedpopulation.php

--- add_affectedpopulation.php
+++ add_affectedpopulation.php
@@ -5,15 +5,13 @@ include_once("db.php");
 header('Access-Control-Allow-Origin: *');
 
 $username=$_POST["txt_affected_city"];
+$typhoon=$_POST["txt_affected_typhoon"];
 $barangay=$_POST["txt_affected_barangay"];
 $families=$_POST["txt_affected_families"];
 
+$sql="INSERT INTO tbl_affected_population VALUES (NULL, '".$username."','".$typhoon."', '".$barangay."', '".$families."',CURRENT_TIMESTAMP);";
 
-
-$sql="INSERT INTO tbl_affected_population VALUES (NULL, '".$username."', '".$barangay."', '".$families."',CURRENT_TIMESTAMP);";
-//$result = mysql_query($sql);
-	
-if (!mysql_query($sql))
+if (!mysqli_query($conn,$sql))
   {
   echo 'Data was not submitted, please check your internet connection';
   echo http_response_code();
