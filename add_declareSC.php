bd97a58a7db95917f835b4a16a79a75401392ba3
sharmaine manuel <201010305@gordoncollegeccs.edu.ph>
2015-10-06 20:18:32 +0800

    Update add_declareSC.php

--- add_declareSC.php
+++ add_declareSC.php
@@ -5,15 +5,13 @@ include_once("db.php");
 header('Access-Control-Allow-Origin: *');
 
 $username=$_POST["txt_calamity_city"];
+$typhoon=$_POST["txt_calamity_typhoon"];
 $location=$_POST["txt_calamity_location"];
 $reso_num=$_POST["txt_calamity_resonum"];
 
+$sql="INSERT INTO tbl_declaresc VALUES (NULL, '".$username."','".$typhoon."', '".$location."', '".$reso_num."',CURRENT_TIMESTAMP);";
 
-
-$sql="INSERT INTO tbl_declaresc VALUES (NULL, '".$username."', '".$location."', '".$reso_num."',CURRENT_TIMESTAMP);";
-//$result = mysql_query($sql);
-	
-if (!mysql_query($sql))
+if (!mysqli_query($conn,$sql))
   {
   echo 'Data was not submitted, please check your internet connection';
   echo http_response_code();
