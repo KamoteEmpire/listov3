a44f6b01b593e2f9ef6beaa803b2a1829b5688b6
sharmaine manuel <201010305@gordoncollegeccs.edu.ph>
2015-10-06 20:19:05 +0800

    Update add_initialdisaster.php

--- add_initialdisaster.php
+++ add_initialdisaster.php
@@ -4,18 +4,27 @@ include_once("db.php");
 
 header('Access-Control-Allow-Origin: *');
 
+
+$month=$_POST["txt_initd_month"];
+$day=$_POST["txt_initd_day"];
+$year=$_POST["txt_initd_year"];
+
+$incident_array = array($year,$month,$day);
+
+
 $username=$_POST["txt_initd_city"];
-$date=$_POST["txt_initd_date"];
+$typhoon=$_POST["txt_initd_typhoon"];
+$date= implode("-",$incident_array);
 $subject=$_POST["txt_initd_subject"];
 $incident=$_POST["txt_initd_incident"];
 $where=$_POST["txt_initd_where"];
 $actions=$_POST["txt_initd_actions"];
 
 
-$sql="INSERT INTO tbl_initial_disaster VALUES (NULL, '".$username."', '".$date."', '".$subject."', '".$incident."','".$where."','".$actions."',CURRENT_TIMESTAMP);";
+$sql="INSERT INTO tbl_initial_disaster VALUES (NULL, '".$username."','".$typhoon."', '".$date."', '".$subject."', '".$incident."','".$where."','".$actions."',CURRENT_TIMESTAMP);";
 //$result = mysql_query($sql);
 	
-if (!mysql_query($sql))
+if (!mysqli_query($conn,$sql))
   {
   echo 'Data was not submitted, please check your internet connection';
   echo http_response_code();
